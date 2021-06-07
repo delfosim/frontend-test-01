3e63549d174f618648f314ae601d6ededc6e7fc1
lpeil <luan@peil.dev>
2021-06-07 20:22:44 -0300

    feat(home screen): get widgets from store

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Grid } from '@material-ui/core';
 
 import { WidgetCard } from '../../components';
 
-const HomePage = () => (
-  <Grid container justify="center">
-    <WidgetCard
-      type="spline"
-      yText="Teste 123"
-      series={[
-        {
-          name: 'Teste',
-          data: [1, 2, 1, 4, 3, 6],
-        },
-        {
-          name: 'Teste 2',
-          data: [4, 2, 12, 3, 2, 0],
-        },
-      ]}
-    />
-  </Grid>
-);
+const HomePage = () => {
+  const widgets = useSelector((state) => state.widgets);
+
+  return (
+    <Grid container direction="column">
+      {widgets && widgets.map((widget, key) => (
+        <Grid container item key={key} xs={12} justify="center">
+          <WidgetCard
+            title={widget.name}
+            type={widget.type}
+            yTitle={widget.yTitle}
+            series={widget.series}
+            xCategories={widget.xCategories}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default HomePage;
--- src/screens/Widget/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const WidgetPage = () => <h1>Widget</h1>;
-
-export default WidgetPage;
