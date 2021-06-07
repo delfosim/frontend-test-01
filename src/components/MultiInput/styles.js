ed990bfe9ad749da59249b0bbff8670be4c0cde3
lpeil <luan@peil.dev>
2021-06-07 20:23:16 -0300

    feat: create multi input component

---
+++ src/components/MultiInput/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  items: {
+    marginTop: '4px',
+  },
+}));
+
+export default useStyles;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
+import FabWidget from './FabWidget';
+import MultiInput from './MultiInput';
 import Navbar from './Navbar';
 import WidgetCard from './WidgetCard';
 
 export {
+  FabWidget,
+  MultiInput,
   Navbar,
   WidgetCard,
 };
